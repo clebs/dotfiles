9637e20d9d09f4e179529d9f1ebe7ee606400088
Borja <borja.clemente@gmail.com>
2023-01-03 10:57:25 +0100

    Backup configuration

--- vim/nvim/lua/keybindings.lua
+++ vim/nvim/lua/keybindings.lua
@@ -10,7 +10,7 @@ function map(mode, lhs, rhs, opts)
 end
 
 -- Debugger settings 
-map("n", "<F5>", ":lua require'dap'.continue()<CR>", {silent = true})
+map("n", "<F5>", ":lua require('dap').continue()<CR>", {silent = true})
 map("n", "<F10>", ":lua require('dap').step_over()<CR>", {silent = true})
 map("n", "<F11>", ":lua require('dap').step_into()<CR>", {silent = true})
 map("n", "<F12>", ":lua require('dap').step_out()<CR>", {silent = true})
@@ -19,7 +19,8 @@ map("n", "<Leader>B", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoi
 map("n", "<Leader>lp", ":lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", {silent = true})
 map("n", "<Leader>dr", ":lua require('dap').repl.open()<CR>", {silent = true})
 map("n", "<Leader>dl", ":lua require('dap').run_last()<CR>ngs", {silent = true})
-map("n", "<Leader>u", ":lua require('dapui').toggle()<CR>", {silent = true})
+
+map('n', "<Leader>u", ":lua require('dapui').toggle()<CR>", {silent = true})
 
 -- Telescope
 local builtin = require('telescope.builtin')
