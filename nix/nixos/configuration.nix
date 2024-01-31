25d8631ce1091b149480f1ebfdd1eebc50770047
Borja <borja.clemente@gmail.com>
2024-01-31 09:31:32 +0100

    Update sample nix config
    Signed-off-by: Borja <borja.clemente@gmail.com>

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -1,36 +1,36 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
- 
+
 { config, pkgs, ... }:
- 
+
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
- 
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_3_10;
- 
+  # set kernel version
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_7;
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
- 
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
- 
+
   # Enable networking
   networking.networkmanager.enable = true;
- 
+
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
- 
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
- 
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
@@ -42,23 +42,23 @@
     LC_TELEPHONE = "de_DE.UTF-8";
     LC_TIME = "de_DE.UTF-8";
   };
- 
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
- 
+
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
- 
+
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "euro";
   };
- 
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
- 
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
@@ -70,15 +70,15 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
- 
+
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
- 
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
- 
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.borja = {
     isNormalUser = true;
@@ -90,27 +90,37 @@
     #  thunderbird
     ];
   };
- 
+
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "borja";
- 
+
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
- 
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
- 
+
   #Allow to use the new nix command
   nix.settings.experimental-features = [ "nix-command" "flakes" ]; 
- 
+  virtualisation.vmware.guest = {
+    headless = true;
+    enable = true; 
+  };
+  
   programs.zsh.enable = true;
- 
+
+  environment.sessionVariables = {
+    # Prevent invisible cursor
+    WLR_NO_HARDWARE_CURSORS = "1"; 
+    # Hint electron apps to use Wayland
+    NIXOS_OZONE_WL = "1";
+  };
+
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # Add unstable channel and update before this:
-  # TODO: how to add channels declaratively (fetchTarball?)
   environment.systemPackages = let unstable = import <nixos-unstable> {};
   in with pkgs; [
   unstable.neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
@@ -132,7 +142,13 @@
   htop
   gnupg
   ];
- 
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = ["FiraCode"];})
+    nerdfonts
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -140,18 +156,18 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
- 
+
   # List services that you want to enable:
- 
+
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
- 
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
- 
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -159,5 +175,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
- 
+
 }
