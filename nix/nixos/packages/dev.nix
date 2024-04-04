4e474f81c402c2189e71bec120e32156bcac8a7d
Borja <borja.clemente@gmail.com>
2024-04-04 17:29:37 +0200

    Fix wrong file naming
    Signed-off-by: Borja <borja.clemente@gmail.com>

--- nix/nixos/packages/dev.nix
+++ nix/nixos/packages/dev.nix
@@ -1,5 +1,7 @@
 {pkgs, ...}: with pkgs; 
 [
-  ventoy
-  gparted
+  go
+  nodejs
+  tree-sitter
+  cargo
 ]
--- nix/nixos/packages/utils.nix
+++ nix/nixos/packages/utils.nix
@@ -1,7 +1,5 @@
 {pkgs, ...}: with pkgs; 
 [
-  go
-  nodejs
-  tree-sitter
-  cargo
+  ventoy
+  gparted
 ]
