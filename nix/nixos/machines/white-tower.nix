fdca7d3bfa11f919f06bf211911eae756bcced21
Borja Clemente <borja.clemente@gmail.com>
2024-04-08 00:38:07 +0200

    NixOS: fix white tower audio crackling
    Signed-off-by: Borja Clemente <borja.clemente@gmail.com>

--- nix/nixos/machines/white-tower.nix
+++ nix/nixos/machines/white-tower.nix
@@ -111,22 +111,22 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  # Enable sound with pipewire.
+  # Enable sound with default settings, pipewire causes crackling.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+  # hardware.pulseaudio.enable = false;
+  # security.rtkit.enable = true;
+  # services.pipewire = {
+    # enable = false;
+    # alsa.enable = true;
+    # alsa.support32Bit = true;
+    # pulse.enable = true;
+    # # If you want to use JACK applications, uncomment this
+    # #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+    # # use the example session manager (no others are packaged yet so this is enabled by default,
+    # # no need to redefine it in your config for now)
+    # #media-session.enable = true;
+  # };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
