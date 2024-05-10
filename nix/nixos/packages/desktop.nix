c0bc82780afd246188953352d72955452f29710a
Borja Clemente <borja.clemente@gmail.com>
2024-05-10 11:43:54 +0200

    NixOS: Gnome bar vitals
    Signed-off-by: Borja Clemente <borja.clemente@gmail.com>

--- nix/nixos/packages/desktop.nix
+++ nix/nixos/packages/desktop.nix
@@ -2,6 +2,7 @@
 [
     gnomeExtensions.dash-to-dock
     gnomeExtensions.blur-my-shell
+    gnomeExtensions.vitals
     # Change border color: dconf write /org/gnome/shell/extensions/pop-shell/hint-color-rgba "'rgba(0, 171, 146, 0.8)'"
     unstable.gnomeExtensions.pop-shell
     unstable.pop-launcher
