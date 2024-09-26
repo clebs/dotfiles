926014d7efa78f406c0e1730a04c6968dd363966
Borja Clemente <borja.clemente@gmail.com>
2024-09-26 20:04:15 +0200

    Update VMW nixos
    Signed-off-by: Borja Clemente <borja.clemente@gmail.com>

--- nix/nixos/machines/arm-vmware.nix
+++ nix/nixos/machines/arm-vmware.nix
@@ -15,7 +15,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   # set kernel version
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_8;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_11;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
