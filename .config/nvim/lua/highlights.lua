cf61e3813be5b49b40bd443f282f17a12ec4dc6d
Borja <borja.clemente@gmail.com>
2025-01-31 09:52:01 +0100

    Neovim: add more comment highlights
    Signed-off-by: Borja <borja.clemente@gmail.com>

--- .config/nvim/lua/highlights.lua
+++ .config/nvim/lua/highlights.lua
@@ -1,4 +1,8 @@
 vim.api.nvim_set_hl(0, "@comment.bonfire", { bg = "#26b5a0", fg = "#2e2e2e" })
+vim.api.nvim_set_hl(0, "@comment.bug", { bg = "#c23a1b", fg = "#f2ba13" })
+vim.api.nvim_set_hl(0, "@comment.note", { bg = "#3069b8", fg = "#52bf26" })
+vim.api.nvim_set_hl(0, "@comment.fixme", { bg = "#dbd827", fg = "#1f1f1f" })
+vim.api.nvim_set_hl(0, "@comment.xxx", { bg = "#53256e", fg = "#3073e6" })
 
 require 'nvim-treesitter.configs'.setup {
   -- Minimal parsers
--- .config/nvim/queries/comment/highlights.scm
+++ .config/nvim/queries/comment/highlights.scm
@@ -11,3 +11,47 @@
 ("text" @comment.bonfire @nospell
   (#eq? @comment.bonfire "BONFIRE"))
 
+((tag
+  (name) @comment.bug @nospell
+  ("(" @punctuation.bracket
+    (user) @constant
+    ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#eq? @comment.bug "BUG"))
+
+("text" @comment.bug @nospell
+  (#eq? @comment.bug "BUG"))
+
+((tag
+  (name) @comment.note @nospell
+  ("(" @punctuation.bracket
+    (user) @constant
+    ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#eq? @comment.note "NOTE"))
+
+("text" @comment.note @nospell
+  (#eq? @comment.note "NOTE"))
+
+((tag
+  (name) @comment.fixme @nospell
+  ("(" @punctuation.bracket
+    (user) @constant
+    ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#eq? @comment.fixme "FIXME"))
+
+("text" @comment.fixme @nospell
+  (#eq? @comment.fixme "FIXME"))
+
+((tag
+  (name) @comment.xxx @nospell
+  ("(" @punctuation.bracket
+    (user) @constant
+    ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#eq? @comment.xxx "XXX"))
+
+("text" @comment.xxx @nospell
+  (#eq? @comment.xxx "XXX"))
+
