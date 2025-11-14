bc968e5ab83c08d129a7e6044e3a703ba937496a
Borja Clemente <borja.clemente@gmail.com>
2025-11-14 14:37:44 +0100

    NixOS: add note on sandbox path permissions
    Signed-off-by: Borja Clemente <borja.clemente@gmail.com>

--- nix/nixos/modules/vpn.nix
+++ nix/nixos/modules/vpn.nix
@@ -11,5 +11,8 @@
   };
 
   # The CA path needs to be added to the build command: `--option extra-sandbox-paths '/usr/local/certs/2022-IT-Root-CA.pem'`
+  # The target folder needs to chown:
+  # chown -R root:nixbld
+  # exec permission is needed in the folder.
   security.pki.certificateFiles = [ "/usr/local/certs/2022-IT-Root-CA.pem" ];
 }
