d79d78363ccf5871f7354a4f303261671a5fa25f
Borja Clemente <borja.clemente@gmail.com>
2026-08-21 14:28:56 +0200

    neovim: configure syntax based folding
    Signed-off-by: Borja Clemente <borja.clemente@gmail.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -100,14 +100,16 @@ opt.title = true                              -- Show file title
 opt.wildmenu = true                           -- Show a more advance menu
 opt.background = 'dark'
 -- opt.cc = 80 -- Show at 80 column a border for good code style
-opt.spell = false         -- enable spell check (may need to download language package)
-opt.ttyfast = true        -- Speed up scrolling in Vim
-opt.ignorecase = true     -- Ignore case on search
-opt.smartcase = true      -- Search is case insensitive when all lowerccase
-opt.foldenable = false    -- Disable folding (smehow neorg set everything to fold)
+opt.spell = false                                -- enable spell check (may need to download language package)
+opt.ttyfast = true                               -- Speed up scrolling in Vim
+opt.ignorecase = true                            -- Ignore case on search
+opt.smartcase = true                             -- Search is case insensitive when all lowerccase
 -- opt.conceallevel = 2      -- conceal level for org mode
-opt.concealcursor = "nvc" -- conceal cursor for org mode
-opt.diffopt = "vertical"  -- vertical split on diff tool
+opt.concealcursor = "nvc"                        -- conceal cursor for org mode
+opt.diffopt = "vertical"                         -- vertical split on diff tool
+opt.foldmethod = "expr"                          -- change fold from vim markers to expressions
+opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()' -- use treesitter expressions to fold based on syntax (e.g. headers or method blocks)
+opt.foldlevel = 999999                           -- set a very high folding level so everything starts unolded by default
 
 -- Git blamer
 g.blamer_show_in_visual_modes = 0
